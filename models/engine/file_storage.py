2766fe66f24b6ab50f364a34d919c379e44ef3d1
Abimbola <abimbolamichael100@gmail.com>
2023-08-13 04:01:42 -0700

    Update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,13 @@
 """a class FileStorage that serializes instances """
 
 import json
-from models.base_model import BaseModel
+"""from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from models.review import Review
+from models.review import Review"""
 
 
 class FileStorage:
