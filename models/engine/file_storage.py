f5e854cfa1c836de426b88154f4cd48563f770ad
Abimbola <abimbolamichael100@gmail.com>
2023-08-13 06:29:29 -0700

    Update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,7 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        
+
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
